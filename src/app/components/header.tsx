a9ebd66e5e1031dddb07ebcde0dadf5c9da50ca3
Vinicius Ferreira <viniciuferreira32@gmail.com>
2024-01-10 09:38:12 -0300

    feat: Component: Header

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -1,3 +1,41 @@
+import Link from 'next/link'
+import { Search, ShoppingBag } from 'lucide-react'
+import Image from 'next/image'
+
 export function Header() {
-  return <div>Header</div>
+  return (
+    <header className="flex items-center justify-between">
+      <div className="flex items-center gap-5">
+        <Link className="text-2xl font-extrabold text-white" href="/">
+          devstore
+        </Link>
+        <form className="flex w-[320px] items-center gap-3 rounded-full bg-zinc-900 px-5 py-3 ring-zinc-700">
+          <Search className="h-5 w-5" />
+          <input
+            placeholder="Buscar produtos..."
+            className="flex-1 bg-transparent text-sm outline-none placeholder:text-zinc-500"
+          />
+        </form>
+      </div>
+      <div className="flex items-center gap-4">
+        <div className="flex items-center gap-2">
+          <ShoppingBag className="h-4 w-4" />
+          <span className="text-sm">Cart 0</span>
+        </div>
+
+        <div className="h-4 w-px bg-zinc-700" />
+
+        <Link href="/" className="flex items-center gap-2 hover:underline">
+          <span>Account</span>
+          <Image
+            src="https://github.com/viniciusferreira7.png"
+            className="h-6 w-6 rounded-full"
+            width={24}
+            height={24}
+            alt=""
+          />
+        </Link>
+      </div>
+    </header>
+  )
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,9 @@ export default function RootLayout({
 }) {
   return (
     <html className={inter.variable} lang="pt">
-      <body>{children}</body>
+      <body className=" flex bg-zinc-950 text-zinc-50 antialiased">
+        {children}
+      </body>
     </html>
   )
 }
