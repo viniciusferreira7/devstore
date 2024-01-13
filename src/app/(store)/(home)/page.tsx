c75fad3d1456493366190bbf19d3e39291cbe85a
Vinicius Ferreira <viniciuferreira32@gmail.com>
2024-01-13 13:19:53 -0300

    feat: Fetch products from Home

--- src/app/(store)/(home)/page.tsx
+++ src/app/(store)/(home)/page.tsx
@@ -1,19 +1,30 @@
 import { api } from '@/data/api'
+import { Product } from '@/data/types/product'
 import Image from 'next/image'
 import Link from 'next/link'
 
-export default function Home() {
-  api('/products')
+async function getFeaturedProducts(): Promise<Product[]> {
+  const response = await api('/products/featured', {
+    cache: 'no-cache',
+  })
+
+  const products = await response.json()
+
+  return products
+}
+
+export default async function Home() {
+  const [highlightedProduct, ...othersProducts] = await getFeaturedProducts()
 
   return (
     <div className="grid-rows-6 grid max-h-[860px] grid-cols-9 gap-6">
       <Link
-        href="/"
+        href={`/product/${highlightedProduct.slug}`}
         className="group relative col-span-6 row-span-6 flex items-baseline justify-center overflow-hidden rounded-lg bg-zinc-900
         "
       >
         <Image
-          src="/moletom-never-stop-learning 1.png"
+          src={highlightedProduct.image}
           className="transition-transform duration-500 group-hover:scale-105"
           width={920}
           height={920}
@@ -21,52 +32,37 @@ export default function Home() {
           alt=""
         />
         <div className="absolute bottom-28 right-28 flex h-12 max-w-[280px] items-center gap-2 rounded-full border-2 border-zinc-500 bg-black/60 p-1 pl-5">
-          <span className="truncate">Moletom never stop learning</span>
+          <span className="truncate">{highlightedProduct.description}</span>
           <span className="flex h-full items-center justify-center rounded-full bg-violet-500 px-4 font-semibold">
-            R$129
+            {highlightedProduct.price}
           </span>
         </div>
       </Link>
-      <Link
-        href="/"
-        className="group relative col-span-3 row-span-3 flex items-baseline justify-center overflow-hidden rounded-lg bg-zinc-900
-        "
-      >
-        <Image
-          src="/moletom-java.png"
-          className="transition-transform duration-500 group-hover:scale-105"
-          width={920}
-          height={920}
-          quality={100}
-          alt=""
-        />
-        <div className="absolute bottom-10 right-28 flex h-12 max-w-[280px] items-center gap-2 rounded-full border-2 border-zinc-500 bg-black/60 p-1 pl-5">
-          <span className="truncate">Moletom Java</span>
-          <span className="flex h-full items-center justify-center rounded-full bg-violet-500 px-4 font-semibold">
-            R$129
-          </span>
-        </div>
-      </Link>
-      <Link
-        href="/"
-        className="group relative col-span-3 row-span-3 flex items-baseline justify-center overflow-hidden rounded-lg bg-zinc-900
-        "
-      >
-        <Image
-          src="/camiseta-dowhile-2022.png"
-          className="transition-transform duration-500 group-hover:scale-105"
-          width={920}
-          height={920}
-          quality={100}
-          alt=""
-        />
-        <div className="absolute bottom-10 right-28 flex h-12 max-w-[280px] items-center gap-2 rounded-full border-2 border-zinc-500 bg-black/60 p-1 pl-5">
-          <span className="truncate">Camiseta dowhile</span>
-          <span className="flex h-full items-center justify-center rounded-full bg-violet-500 px-4 font-semibold">
-            R$99
-          </span>
-        </div>
-      </Link>
+      {othersProducts.slice(0, 2).map((product) => {
+        return (
+          <Link
+            key={product.id}
+            href={`/product/${product.slug}`}
+            className="group relative col-span-3 row-span-3 flex items-baseline justify-center overflow-hidden rounded-lg bg-zinc-900
+          "
+          >
+            <Image
+              src={product.image}
+              className="transition-transform duration-500 group-hover:scale-105"
+              width={920}
+              height={920}
+              quality={100}
+              alt=""
+            />
+            <div className="absolute bottom-10 right-28 flex h-12 max-w-[280px] items-center gap-2 rounded-full border-2 border-zinc-500 bg-black/60 p-1 pl-5">
+              <span className="truncate">{product.description}</span>
+              <span className="flex h-full items-center justify-center rounded-full bg-violet-500 px-4 font-semibold">
+                {product.price}
+              </span>
+            </div>
+          </Link>
+        )
+      })}
     </div>
   )
 }
--- src/app/api/products/featured/route.ts
+++ src/app/api/products/featured/route.ts
@@ -1,7 +1,19 @@
 import data from '../data.json'
 
 export async function GET() {
-  const featuredProducts = data.products.filter((product) => product.featured)
+  const featuredProducts = data.products
+    .filter((product) => product.featured)
+    .map((product) => {
+      return {
+        ...product,
+        price: product.price.toLocaleString('pt-br', {
+          style: 'currency',
+          currency: 'BRL',
+          minimumFractionDigits: 0,
+          maximumFractionDigits: 0,
+        }),
+      }
+    })
 
-  return Response.json({ featuredProducts })
+  return Response.json(featuredProducts)
 }
