e89b743e656e26a81156b7e9ec0a74a6324ea4ec
Vinicius Ferreira <viniciuferreira32@gmail.com>
2024-01-20 07:07:56 -0300

    feat: Page Metadata in Next.js (SEO)

--- src/app/(store)/(home)/page.tsx
+++ src/app/(store)/(home)/page.tsx
@@ -1,5 +1,6 @@
 import { api } from '@/data/api'
 import { Product } from '@/data/types/product'
+import { Metadata } from 'next'
 import Image from 'next/image'
 import Link from 'next/link'
 
@@ -16,11 +17,15 @@ async function getFeaturedProducts(): Promise<Product[]> {
   return products
 }
 
+export const metadata: Metadata = {
+  title: 'Home',
+}
+
 export default async function Home() {
   const [highlightedProduct, ...othersProducts] = await getFeaturedProducts()
 
   return (
-    <div className="grid-rows-6 grid max-h-[860px] grid-cols-9 gap-6">
+    <div className="grid max-h-[860px] grid-cols-9 grid-rows-6 gap-6">
       <Link
         href={`/product/${highlightedProduct.slug}`}
         className="group relative col-span-6 row-span-6 flex items-baseline justify-center overflow-hidden rounded-lg bg-zinc-900
--- src/app/(store)/layout.tsx
+++ src/app/(store)/layout.tsx
@@ -1,5 +1,13 @@
 import { Header } from '@/app/components/header'
+import { Metadata } from 'next'
 import { ReactNode } from 'react'
+
+export const metadata: Metadata = {
+  title: {
+    template: '%s | devstore',
+    default: 'devstore',
+  },
+}
 
 export default function StoreLayout({ children }: { children: ReactNode }) {
   return (
