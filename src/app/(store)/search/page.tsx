435c4d75c23a34f7b2a039fbe74ead9774a23d9a
Vinicius Ferreira <viniciuferreira32@gmail.com>
2024-01-27 07:22:17 -0300

    feat: page to product search

--- src/app/(store)/search/page.tsx
+++ src/app/(store)/search/page.tsx
@@ -1,3 +1,41 @@
+import Image from 'next/image'
+import Link from 'next/link'
+
 export default function Search() {
-  return <h1>Hello world</h1>
+  return (
+    <div className="flex flex-col gap-4">
+      <p className="text-sm">
+        Resultado para: <span className="font-semibold">moletom</span>
+      </p>
+
+      <div className="grid grid-cols-3 gap-6">
+        <Link
+          href="/product/moletom-never-stop-learning"
+          className="group relative flex items-baseline justify-center overflow-hidden rounded-lg bg-zinc-900
+          "
+        >
+          <Image
+            src="/moletom-never-stop-learning.png"
+            className="transition-transform duration-500 group-hover:scale-105"
+            width={920}
+            height={920}
+            quality={100}
+            alt=""
+          />
+          <div className="absolute bottom-10 right-28 flex h-12 max-w-[280px] items-center gap-2 rounded-full border-2 border-zinc-500 bg-black/60 p-1 pl-5">
+            <span className="truncate">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Voluptates nesciunt explicabo quidem laudantium rem accusamus
+              molestiae tenetur libero porro culpa voluptate similique,
+              voluptatem velit assumenda omnis labore exercitationem voluptas
+              fugiat.
+            </span>
+            <span className="flex h-full items-center justify-center rounded-full bg-violet-500 px-4 font-semibold">
+              R$120
+            </span>
+          </div>
+        </Link>
+      </div>
+    </div>
+  )
 }
--- src/app/api/products/search/route.ts
+++ src/app/api/products/search/route.ts
@@ -7,9 +7,21 @@ export async function GET(request: NextRequest) {
 
   const query = z.string().parse(searchParams.get('q'))
 
-  const product = data.products.filter((product) =>
-    product.title.toLocaleLowerCase().includes(query.toLocaleLowerCase()),
-  )
+  const product = data.products
+    .filter((product) =>
+      product.title.toLocaleLowerCase().includes(query.toLocaleLowerCase()),
+    )
+    .map((product) => {
+      return {
+        ...product,
+        price: product.price.toLocaleString('pt-br', {
+          style: 'currency',
+          currency: 'BRL',
+          minimumFractionDigits: 0,
+          maximumFractionDigits: 0,
+        }),
+      }
+    })
 
   if (!product) {
     return Response.json({ message: 'Product not found' }, { status: 400 })
