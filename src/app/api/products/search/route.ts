06707f235bf7532b812e7dd18b60d217e33b9725
Vinicius Ferreira <viniciuferreira32@gmail.com>
2024-01-27 07:05:45 -0300

    feat: endpoint to Product search

---
+++ src/app/api/products/search/route.ts
@@ -0,0 +1,18 @@
+import { z } from 'zod'
+import data from '../data.json'
+import { NextRequest } from 'next/server'
+
+export async function GET(request: NextRequest) {
+  const { searchParams } = request.nextUrl
+
+  const query = z.string().parse(searchParams.get('q'))
+
+  const product = data.products.filter((product) =>
+    product.title.toLocaleLowerCase().includes(query.toLocaleLowerCase()),
+  )
+
+  if (!product) {
+    return Response.json({ message: 'Product not found' }, { status: 400 })
+  }
+  return Response.json(product)
+}
